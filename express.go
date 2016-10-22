79db2e4dc9c83e4c482534c35af066cabe4af53c
Icebob <mereg.norbert@gmail.com>
2016-10-22 15:13:06 +0200

    remove log messages

--- express.go
+++ express.go
@@ -1,29 +1,31 @@
-// Package goexpress provides the actual hook that 
+// Package goexpress provides the actual hook that
 // enables you to start building your application.
-// 
+//
 // The basic Express() functions returns an instance
 // for the express which can be further be used as
 // an express hook.
-// 
+//
 // app.Use(), app.Get(), app.Post(), app.Delete(), app.Push()
 // app.Put() are the top level functions that can be used in
 // the same fashion as the express-js ones are.
 package goexpress
+
 import (
-	"log"
 	http "net/http"
-	response "github.com/DronRathore/goexpress/response"
-	request "github.com/DronRathore/goexpress/request"
-	router "github.com/DronRathore/goexpress/router"
+
+	request "github.com/icebob/goexpress/request"
+	response "github.com/icebob/goexpress/response"
+	router "github.com/icebob/goexpress/router"
 )
 
 type express struct {
-	router *router.Router
-	started bool
+	router     *router.Router
+	started    bool
 	properties map[string]interface{}
 }
+
 // Returns a new instance of express
-func Express() *express{
+func Express() *express {
 	var exp = &express{}
 	exp.router = &router.Router{}
 	exp.router.Init()
@@ -33,7 +35,7 @@ func Express() *express{
 
 // ServeHTTP
 // Default function to handle HTTP request
-func (e *express) ServeHTTP(res http.ResponseWriter,req *http.Request) {
+func (e *express) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	hijack, ok := res.(http.Hijacker)
 	if !ok {
 		http.Error(res, "Request Hijacking not supported for this request", http.StatusInternalServerError)
@@ -51,7 +53,7 @@ func (e *express) ServeHTTP(res http.ResponseWriter,req *http.Request) {
 		var executedRoutes = 0
 		var next func()
 		var _next router.NextFunc
-		_next = func(n router.NextFunc){
+		_next = func(n router.NextFunc) {
 			if response.HasEnded() == true {
 				// we are done
 				return
@@ -83,7 +85,7 @@ func (e *express) ServeHTTP(res http.ResponseWriter,req *http.Request) {
 				}
 			}
 		}
-		next = func () {
+		next = func() {
 			_next(_next)
 		}
 		_next(_next)
@@ -91,62 +93,65 @@ func (e *express) ServeHTTP(res http.ResponseWriter,req *http.Request) {
 }
 
 // Extension to provide Router.Get functionalities
-func (e *express) Get(url string, middleware router.Middleware) *express{
+func (e *express) Get(url string, middleware router.Middleware) *express {
 	e.router.Get(url, middleware)
 	return e
 }
 
 // Extension to provide Router.Post functionality
-func (e *express) Post(url string, middleware router.Middleware) *express{
+func (e *express) Post(url string, middleware router.Middleware) *express {
 	e.router.Post(url, middleware)
 	return e
 }
 
 // Extension to provide Router.Put functionality
-func (e *express) Put(url string, middleware router.Middleware) *express{
+func (e *express) Put(url string, middleware router.Middleware) *express {
 	e.router.Put(url, middleware)
 	return e
 }
 
 // Extension to provide Router.Patch functionality
-func (e *express) Patch(url string, middleware router.Middleware) *express{
+func (e *express) Patch(url string, middleware router.Middleware) *express {
 	e.router.Patch(url, middleware)
 	return e
 }
 
 // Extension to provide Router.Delete functionality
-func (e *express) Delete(url string, middleware router.Middleware) *express{
+func (e *express) Delete(url string, middleware router.Middleware) *express {
 	e.router.Delete(url, middleware)
 	return e
 }
 
 // Extension to provide Router.Use functionality
-func (e *express) Use(middleware router.Middleware) *express{
+func (e *express) Use(middleware router.Middleware) *express {
 	e.router.Use(middleware)
 	return e
 }
 
 // Sets global app properties that can be accessed under express struct
-func (e *express) SetProp(key string, value interface{}) *express{
+func (e *express) SetProp(key string, value interface{}) *express {
 	e.properties[key] = value
 	return e
 }
 
 // Return the app property
-func (e *express) GetProp(key string, value interface{}) interface{}{
+func (e *express) GetProp(key string, value interface{}) interface{} {
 	return e.properties[key]
 }
 
 // Starts the App Server
-func (e *express) Start(port string) *express{
+func (e *express) Start(port string) *express {
+	return e.Listen(port, "0.0.0.0")
+}
+
+// Start listening
+func (e *express) Listen(port string, ip string) *express {
 	if e.started {
 		return e
 	}
-	log.Print("Listening at: ", port)
-	err := http.ListenAndServe("0.0.0.0:" + port, e)
+	err := http.ListenAndServe(ip+":"+port, e)
 	if err != nil {
-		log.Fatal("Listen Error:", err)
+		e.started = true
 	}
-	e.started = true
 	return e
 }
