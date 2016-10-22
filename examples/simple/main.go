5ed2dde2d8dd6f5c17a1afe8c074260f00195691
Icebob <mereg.norbert@gmail.com>
2016-10-22 22:09:59 +0200

    improve test

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -2,33 +2,38 @@ package main
 
 import (
 	"log"
-	"time"
 
 	express "github.com/icebob/goexpress"
 	"github.com/icebob/goexpress/request"
 	"github.com/icebob/goexpress/response"
-
-	logger "github.com/icebob/goexpress/middlewares/log"
+	// logger "github.com/icebob/goexpress/middlewares/log"
 )
 
 func main() {
+	//runtime.GOMAXPROCS(8)
+
 	var app = express.Express()
 
-	app.Use(logger.Simple)
+	// app.Use(logger.Simple)
 
 	app.Get("/test", func(req *request.Request, res *response.Response, next func()) {
-		res.Write("Hello Test")
+		res.Send("Hello Test")
+		// you can skip closing connection
+	})
+
+	app.Get("/chunked", func(req *request.Request, res *response.Response, next func()) {
+		res.WriteChunk("Hello World\n")
+		//time.Sleep(5 * time.Second)
+		res.WriteChunk("Hello World2\n")
 		// you can skip closing connection
 	})
 
 	app.Get("/", func(req *request.Request, res *response.Response, next func()) {
-		res.Write("Hello World\n")
-		time.Sleep(5 * time.Second)
-		res.Write("Hello World2\n")
-		// you can skip closing connection
+		res.Send("Hello World")
 	})
 
-	bindAddress, bindPort := "127.0.0.1", 3000
+	//bindAddress, bindPort := "127.0.0.1", 3000
+	bindAddress, bindPort := "0.0.0.0", 3000
 	log.Printf("Listening at %s:%d...\n", bindAddress, bindPort)
 	err := app.Listen(bindPort, bindAddress)
 	if err != nil {
